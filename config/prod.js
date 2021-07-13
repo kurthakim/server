901ef5741e7f8404f925768a7f8e00b5e230ab70
Kurt Hakim <kurthakim@gmail.com>
2021-07-13 17:13:25 +0700

    include prod

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+// prod.js - production keys here!!// devs.js - don't commit this!!!
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
+mongouri='mongodb+srv://myuser:SryMiyVW1M1dXsGG@cluster0.jojl1.mongodb.net/prodDatabase?retryWrites=true&w=majority'
+googleClientID='308480231877-6lbl3gpf79f2iftk58int6r32566ohfj.apps.googleusercontent.com'
+googleClientSecret='utxCvp-eO7ER3Ya9xP7Gmmo2'
