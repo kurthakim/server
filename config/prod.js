99e5a78c158bf8f729389863e50fce8234f60ad3
Kurt Hakim <kurthakim@gmail.com>
2021-07-16 20:40:29 +0700

    Finish authentication and starting on billing

--- config/prod.js
+++ config/prod.js
@@ -1,13 +1,11 @@
-// prod.js - production keys here!!// devs.js - don't commit this!!!
+// prod.js - production keys here!!
 
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
 
-
-mongouri='mongodb+srv://myuser:SryMiyVW1M1dXsGG@cluster0.jojl1.mongodb.net/prodDatabase?retryWrites=true&w=majority'
-googleClientID='308480231877-6lbl3gpf79f2iftk58int6r32566ohfj.apps.googleusercontent.com'
-googleClientSecret='utxCvp-eO7ER3Ya9xP7Gmmo2'
